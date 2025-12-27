2809cc2f0512913d3e92598ba4286875ec89e2dd
Mahendran Sadasivam <sanjayvikramcb@gmail.com>
2025-12-27 10:23:03 +0530

    .

--- backend/prediction.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-model = YOLO('results/weights/best.pt')  # load best model
-results = model(r'E:\leaf\dataset\rose\rose_014.png')     # predict
-print(results[0].probs.top1)
--- backend/train_yolo.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-if __name__ == '__main__':
-    # Load a model
-    model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for training)
-
-    # Train the model
-    results = model.train(data='data', epochs=20, imgsz=224, batch=16, project='.', name='results')
